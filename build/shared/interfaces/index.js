df6bd35990be7bfed4103fdf8e975178dc7a4b9a
Aleksandr Korsukov <alexask.korsukov@gmail.com>
2020-12-14 19:23:09 -0500

    changes in .gitignore

--- build/shared/interfaces/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ERRORS = void 0;
-var ERRORS;
-(function (ERRORS) {
-    ERRORS["ERROR_100"] = "Data is empty or not provided";
-    ERRORS["ERROR_101"] = "Data format is incorrect";
-})(ERRORS = exports.ERRORS || (exports.ERRORS = {}));
